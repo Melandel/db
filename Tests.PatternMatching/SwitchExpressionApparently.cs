ab19475801223377a3767f12f7e657ee94c97ca0
Minh-Tam TRAN <c_mtran@capsuletech.com>
2023-09-17 23:13:41 +0200

    🚀 feature         (pattern-matching): `switch expressions` apparently cover `null` value in default switch branch, even with `var` syntax

---
+++ Tests.PatternMatching/SwitchExpressionApparently.cs
@@ -0,0 +1,23 @@
+namespace Tests.PatternMatching;
+
+class SwitchExpressionApparently
+{
+	[Test]
+	public void Covers_Null_Value_In_Default_Switch_Branch_With_Var_Syntax()
+	{
+		// Arrange
+		string str = null;
+		// Act
+		var nb = str switch
+		{
+			"a" => 0,
+			"b" => 1,
+			var v when v is { } => 2,
+			var v when v is { Length: 0 } => 3,
+			var v => 4
+		};
+
+		// Assert
+		Assert.That(nb, Is.EqualTo(4));
+	}
+}
