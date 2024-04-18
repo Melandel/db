5e170406264f3b1aaa41ee61720d87ea7dd88f93
Minh-Tam TRAN <c_mtran@capsuletech.com>
2024-04-18 10:47:07 +0200

    🚀 feature         (ranges_and_indexes): `array[..N]` apparently ends including the `Nth` character

--- Tests.Range/RangesApparently.cs
+++ Tests.Range/RangesApparently.cs
@@ -3,7 +3,7 @@ namespace Tests.Range;
 public class RangesApparently
 {
 	[Test]
-	public void Start_From_The_Item_AFTER_The_Nth_Position()
+	public void Start_AFTER_The_Nth_Character()
 	{
 		// Arrange
 		var str = "abcdef";
@@ -14,11 +14,24 @@ public class RangesApparently
 
 		// Assert
 		Assert.That(slice, Is.EqualTo("def"));
-		Assert.That(slice, Is.EqualTo(str.Substring(three)));
 	}
 
 	[Test]
-	public void Is_Equivalent_To_Substring_For_String_Type()
+	public void End_Including_The_Item_AT_The_Nth_Position()
+	{
+		// Arrange
+		var str = "abcdef";
+		var three = "abc".Length;
+
+		// Act
+		var slice = str[1..three];
+
+		// Assert
+		Assert.That(slice, Is.EqualTo("bc"));
+	}
+
+	[Test]
+	public void Is_Equivalent_To_Substring()
 	{
 		// Arrange
 		var str = "abcdef";
@@ -30,5 +43,19 @@ public class RangesApparently
 
 		// Assert
 		Assert.That(slice, Is.EqualTo(substring));
+	}
+
+	[TestCase(1)]
+	[TestCase(9)]
+	public void Throws_When_Index_GreaterThen_StringLength(int offset)
+	{
+		// Arrange
+		var str = "abc";
+		var numberGreaterThanStringLength = str.Length + offset;
+
+		// Act & Assert
+		Assert.That(
+			() => { _ = str[numberGreaterThanStringLength..]; },
+			Throws.InstanceOf<ArgumentOutOfRangeException>());
 	}
 }
